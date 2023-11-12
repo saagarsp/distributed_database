654cea060808ec893d09460341d7fa3e01e7d60f
saagarsp <saagar.201it252@nitk.edu.in>
2023-11-12 17:57:01 +0530

    Inital Commit

---
+++ config.js
@@ -0,0 +1,91 @@
+var config = {}
+
+config.endpoint = 'https://saagar.documents.azure.com:443/'
+config.key = 'bjU08O3WetawSGkCzc7fKGvm40b74v0H2Y9wFuhvzMPjsuOkHXegcmr566iWpaF6A6G6ZdYuOjGTACDb1DNlqg=='
+
+config.database = {
+  id: 'ToDoList'
+}
+
+config.container = {
+  id: 'Items'
+}
+
+config.items = {
+  Andersen: {
+    id: 'Anderson.1',
+    Country: 'USA',
+    partitionKey: 'USA',
+    lastName: 'Andersen',
+    parents: [
+      {
+        firstName: 'Thomas'
+      },
+      {
+        firstName: 'Mary Kay'
+      }
+    ],
+    children: [
+      {
+        firstName: 'Henriette Thaulow',
+        gender: 'female',
+        grade: 5,
+        pets: [
+          {
+            givenName: 'Fluffy'
+          }
+        ]
+      }
+    ],
+    address: {
+      state: 'WA',
+      county: 'King',
+      city: 'Seattle'
+    }
+  },
+  Wakefield: {
+    id: 'saagar',
+    partitionKey: 'Italy',
+    Country: 'Italy',
+    parents: [
+      {
+        familyName: 'Wakefield',
+        firstName: 'Robin'
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Ben'
+      }
+    ],
+    children: [
+      {
+        familyName: 'Merriam',
+        firstName: 'Jesse',
+        gender: 'female',
+        grade: 8,
+        pets: [
+          {
+            givenName: 'Goofy'
+          },
+          {
+            givenName: 'Shadow'
+          }
+        ]
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Lisa',
+        gender: 'female',
+        grade: 1
+      }
+    ],
+    address: {
+      state: 'NY',
+      county: 'Manhattan',
+      city: 'NY'
+    },
+    isRegistered: false
+  }
+}
+
+module.exports = config
